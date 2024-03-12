93a947dbe5577c77e602bd6fbfed0ceb009bd740
U-T-kuroitigo <git.cq.kx@gmail.com>
2024-03-13 00:30:29 +0900

    update/app
    データベースへの接続とサーバーの最適化

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
 from api import app
+from dotenv import load_dotenv
+from flask import request, make_response, jsonify, abort
+import json
+
+load_dotenv()
+import os
+
+PORT = os.getenv("PORT")
 
 
 @app.route("/")
@@ -8,5 +16,13 @@ def index():
     return "It works!"
 
 
+@app.errorhandler(Exception)
+def error_handler(err):
+    res = jsonify(
+        {"error": {"name": err.name, "description": err.description}, "code": err.code}
+    )
+    return res, err.code
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=PORT, threaded="true")
