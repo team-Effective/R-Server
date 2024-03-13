2e624cbfab1edebb852cbbed78d9b1d375aa6f18
U-T-kuroitigo <git.cq.kx@gmail.com>
2024-03-13 16:14:14 +0900

    add/view/player/insert
    playerテーブル用viewのinsertを追加

--- flask_app/api/__init__.py
+++ flask_app/api/__init__.py
@@ -24,8 +24,10 @@ def create_app():
     app.register_blueprint(host_update, url_prefix="/api/host/update")
 
     from .views.player_view.player_select import player_select
+    from .views.player_view.player_insert import player_insert
 
     app.register_blueprint(player_select, url_prefix="/api/player/select")
+    app.register_blueprint(player_insert, url_prefix="/api/player/insert")
 
     return app
 
