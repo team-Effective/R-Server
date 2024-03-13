f8a116afa728291095fa836675c4eb3025d66cb2
U-T-kuroitigo <git.cq.kx@gmail.com>
2024-03-13 14:24:26 +0900

    add/model/player/update
    hostモデルにupdate処理を追加した。

--- flask_app/api/models/player_model.py
+++ flask_app/api/models/player_model.py
@@ -5,19 +5,8 @@ from flask import abort
 class PlayerModel(db.Model):
     __table__ = "player"
 
-    # プレイヤーの全件取得
-    def getPlayerList():
-        try:
-            get_player_list = db.session.query(PlayerModel).all()
-        except Exception as e:
-            abort(400, e.args)
-        if get_player_list == None:
-            return []
-        else:
-            return get_player_list
-
     # プレイヤーの一件取得
-    def getPlayer(requested_player_id):
+    def selectPlayer(requested_player_id):
         try:
             get_player = (
                 db.session.query(PlayerModel)
@@ -32,19 +21,43 @@ class PlayerModel(db.Model):
             return get_player
 
     # プレイヤーの登録
-    def registPlayer(requested_player):
+    def insertPlayer(requested_player):
         try:
-            registering_player = PlayerModel(
+            inserting_player = PlayerModel(
                 player_id=requested_player.get("player_id"),
                 player_name=requested_player.get("player_name"),
-                player_password=requested_player.get("player_password"),
             )
-            db.session.add(registering_player)
+            db.session.add(inserting_player)
             db.session.commit()
         except Exception as e:
             db.session.rollback()
             abort(400, e.args)
-        return registering_player
+        return inserting_player
+
+    # プレイヤーの更新
+    def updateHost(requested_player):
+        try:
+            updating_player = (
+                db.session.query(PlayerModel)
+                .filter(
+                    PlayerModel.__table__.columns.player_id
+                    == requested_player.get("player_id")
+                )
+                .first()
+            )
+            if requested_player.get("player_name") is not None:
+                updating_player.player_name = requested_player.get("player_name")
+            if requested_player.get("match_count") is not None:
+                updating_player.match_count = requested_player.get("match_count")
+            if requested_player.get("alive_count") is not None:
+                updating_player.alive_count = requested_player.get("alive_count")
+            updating_player.now_game = requested_player.get("now_game")
+        except Exception as e:
+            db.session.rollback()
+            abort(400, e.args)
+        db.session.add(updating_player)
+        db.session.commit()
+        return updating_player
 
 
 class PlayerSchema(ma.SQLAlchemyAutoSchema):
